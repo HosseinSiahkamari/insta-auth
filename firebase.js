cc296fad9953b8744e0b5a1061f2475ae7f748d9
Mohammadhossein <siahkamarii1363@gmail.com>
2023-11-11 18:36:55 +0100

    master

--- firebase.js
+++ firebase.js
@@ -7,13 +7,13 @@ import {getStorage} from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDtH0q_ctUTJWCP5ZhJBmcpeEEQvkWp4oc",
-  authDomain: "insta-auth-ca60b.firebaseapp.com",
-  projectId: "insta-auth-ca60b",
-  storageBucket: "insta-auth-ca60b.appspot.com",
-  messagingSenderId: "245078118757",
-  appId: "1:245078118757:web:10a225962c671140f9d18c"
-};
+    apiKey: "AIzaSyCil4QYhVsc1_3Q3oyOsznQEJChVMTFTIE",
+    authDomain: "insta-auth-1a7fa.firebaseapp.com",
+    projectId: "insta-auth-1a7fa",
+    storageBucket: "insta-auth-1a7fa.appspot.com",
+    messagingSenderId: "1086708716778",
+    appId: "1:1086708716778:web:f7dde00b3106415dd33393"
+  };
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
