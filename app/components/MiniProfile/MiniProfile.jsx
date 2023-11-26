708e376d6d3b092177f90e1895c7fe24e4e15a28
Mohammadhossein <siahkamarii1363@gmail.com>
2023-11-26 20:19:14 +0100

    master

--- app/components/MiniProfile/MiniProfile.jsx
+++ app/components/MiniProfile/MiniProfile.jsx
@@ -1,18 +1,27 @@
-
-import { getServerSession, s } from "next-auth/next"
-import { options } from './../../api/auth/[...nextauth]/options'
+'use client'
+import { useSession } from "next-auth/react";
 import SignInButton from "../signInButton/signInButton";
 
 
-const MiniProfile = async () => {
+const MiniProfile = () => {
 
-    // const session = await getServerSession(options);
+    const { data: session } = useSession();
+    const isImageAvailable = session?.user?.image;
+    console.log(isImageAvailable);
+
+    
 
 
     return (
         <div className="flex ml-10 h-12 w-full mt-10">
             <div className="flex justify-start h-12 w-2/3">
-                <img src="https://i.pinimg.com/originals/ac/c7/e2/acc7e2c10f504bb8b923e50420c3e9b8.jpg" className='ml-0 rounded-full h-12 w-12 cursor-pointer' alt='picture ' />
+               {
+                isImageAvailable &&  (<img
+                src={isImageAvailable}
+                className='ml-0 rounded-full h-12 w-12 cursor-pointer'
+                alt='picture'
+            />)
+               }
                 <p className='w-44 h-12 flex items-center text-sm ml-2 font-semibold cursor-pointer'>hossein</p>
             </div>
             <div className="w-1/3 h-12 flex items-center justify-center">
--- app/components/signInButton/signInButton.jsx
+++ app/components/signInButton/signInButton.jsx
@@ -10,12 +10,12 @@ const SignInButton = () => {
     if ( session && session.user) {
         return ( 
             <div>
-                <button onClick={()=> signOut()} className='bg-red-400 rounded-sm px-6 mt-10'>Sign Out</button>
+                <button onClick={()=> signOut()} className='text-red-400 text-xs font-semibold cursor-pointer mr-2 flex items-center justify-end'>Sign Out</button>
             </div>
          );
     }else{
         return (<div>
-            <button onClick={()=> signIn("google")} className='bg-red-400 rounded-sm px-6 mt-10'>Sign In</button>
+            <button onClick={()=> signIn("google")} className='text-red-400 text-xs font-semibold cursor-pointer mr-2 flex items-center justify-end'>Sign In</button>
         </div>)
     }
 }
