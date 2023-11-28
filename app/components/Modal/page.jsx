9b4f73ce7bea75c1537bff092bf7f4c7bfbf289d
Mohammadhossein <siahkamarii1363@gmail.com>
2023-11-28 23:40:19 +0100

    firebase

--- app/components/Modal/page.jsx
+++ app/components/Modal/page.jsx
@@ -1,22 +1,23 @@
-import { Dialog, Transition } from '@headlessui/react'
+
+import React from "react";
+import { Dialog, Transition } from "@headlessui/react";
 import { Fragment, useState, useRef } from "react";
-import { useRecoilState } from "recoil"
-import { modalState } from "@/app/RecoilContextProvider"
+import { useRecoilState } from "recoil";
+import { modalSate } from "./../atoms/modalAtom";
 import { CameraIcon } from "@heroicons/react/outline";
 import {
-  addDoc,
-  collection,
-  doc,
-  serverTimestamp,
-  updateDoc,
+    addDoc,
+    collection,
+    doc,
+    serverTimestamp,
+    updateDoc,
 } from "firebase/firestore";
 import { db, storage } from "../firebase";
 import { useSession } from "next-auth/react";
 import { ref, getDownloadURL, uploadString } from "firebase/storage";
 
 function Modal() {
-
-    const [open, setOpen] = useRecoilState(modalState);
+    const [open, setOpen] = useRecoilState(modalSate);
     const [selectedFile, setSelectedFile] = useState(null);
     const filePickerRef = useRef(null);
     const captionRef = useRef(null);
@@ -27,34 +28,68 @@ function Modal() {
         if (loading) return;
         setLoading(true);
         const docRef = await addDoc(collection(db, "posts"), {
-          username: session.user.username,
-          caption: captionRef.current.value,
-          profileImg: session.user.image,
-          timestamp: serverTimestamp(),
+            username: session.user.username,
+            caption: captionRef.current.value,
+            profileImg: session.user.image,
+            timestamp: serverTimestamp(),
         });
+
+        const imageRef = ref(storage, `posts/${docRef.id}/image`);
+        await uploadString(imageRef, selectedFile, "data_url").then(
+            async (snapshot) => {
+                const downloadURL = await getDownloadURL(imageRef);
+                await updateDoc(doc(db, "posts", docRef.id), {
+                    image: downloadURL,
+                });
+            }
+        );
+
+        setOpen(false);
+        setLoading(false);
+        setSelectedFile(null);
+    };
 
     const addImageToPost = (e) => {
         const reader = new FileReader();
         if (e.target.files[0]) {
-          reader.readAsDataURL(e.target.files[0]);
-        }
-    
+            reader.readAsDataURL(e.target.files[0]);
+        } 
         reader.onload = (readerEvent) => {
-          setSelectedFile(readerEvent.target.result);
+            setSelectedFile(readerEvent.target.result);
         };
-      };
+    }
 
     return (
-        <Transition.Root show={true} as={Fragment} >
-            <Dialog as='div' className='fixed z-50 inset-0 overflow-y-auto' onClose={setOpen} >
+        <Transition.Root show={open} as={Fragment}>
+            <Dialog
+                as="div"
+                className="fixed z-10 inset-0 overflow-y-auto"
+                onClose={setOpen}
+            >
                 <div className="flex items-end justify-center min-h-[800px] sm:min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-                    <Transition.Child as={Fragment}
+                    <Transition.Child
+                        as={Fragment}
                         enter="ease-out duration-300"
                         enterFrom="opacity-0"
                         enterTo="opacity-100"
                         leave="ease-in duration-200"
                         leaveFrom="opacity-100"
                         leaveTo="opacity-0"
+                    >
+                        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                    </Transition.Child>
+                    <span
+                        className="hidden sm:inline-block sm:align-middle sm:h-screen "
+                        aria-hidden="true"
+                    ></span>
+                    <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                        enterTo="opacity-100 translate-y-0 sm:scale-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                        leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                     >
                         <Dialog.Panel className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform translate-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
                             <div>
@@ -115,14 +150,16 @@ function Modal() {
                             </div>
                         </Dialog.Panel>
                     </Transition.Child>
-                    <span
-                        className="hidden sm:inline-block sm:align-middle sm:h-screen "
-                        aria-hidden="true"
-                    ></span>
                 </div>
             </Dialog>
         </Transition.Root>
-    )
-}}
+    );
+}
 
-export default Modal
+export default Modal;
+
+
+    
+
+
+        
