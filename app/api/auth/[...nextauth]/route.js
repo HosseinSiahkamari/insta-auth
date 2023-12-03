4671fac115412064937c5831bc3a594361b2e71d
Mohammadhossein <siahkamarii1363@gmail.com>
2023-12-03 21:00:00 +0100

    master

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -3,23 +3,29 @@
 import {options} from "./options";
 import NextAuth from "next-auth/next";
 
-// export const authOptions = {
-    
-//     providers: [
-//         GoogleProvider({
-//             clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-//             clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-//         }),
-       
-//     ],
-    // callbacks: {
-    //     async session({ session, token, user }) {
-    //         session.user.username = session.user.username.splite(" ").join("").toLowerCase();
-    //         session.user.uid = token.sub;
-    //         return session
-    //     }
-    // }
-// }
+export default NextAuth({
+    // Configure one or more authentication providers
+    // providers: [
+    //   GoogleProvider({
+    //     clientId: process.env.GOOGLE_CLIENT_ID,
+    //     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    //   }),
+    //   // ...add more providers here
+    // ],
+    // pages: {
+    //   signIn: "/auth/signin",
+    // },
+    callbacks: {
+      async session({ session, token, user }) {
+        session.user.username = session.user.name
+          .split(" ")
+          .join("")
+          .toLowerCase();
+        session.user.uid = token.sub;
+        return session;
+      },
+    },
+  });
 
 export const handler = NextAuth(options)
 
